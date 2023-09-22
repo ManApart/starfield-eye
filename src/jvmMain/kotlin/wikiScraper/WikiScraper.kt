2246ff7b859d5bf0f6d2030a2ccd1ce1400a8b41
ManApart <r@austinkucera.com>
2023-09-22 12:34:16 -0400

    wip selecting data

--- src/jvmMain/kotlin/wikiScraper/WikiScraper.kt
+++ src/jvmMain/kotlin/wikiScraper/WikiScraper.kt
@@ -2,7 +2,7 @@ package wikiScraper
 
 import org.jsoup.Jsoup
 
-fun main(){
+fun main() {
     val planets = listOf("Earth")
     planets
         .map { getPage("https://starfieldwiki.net/wiki/Starfield:$it") }
@@ -10,8 +10,15 @@ fun main(){
 }
 
 fun parseWikiData(pageString: String): WikiData {
-    val rows = Jsoup.parse(pageString).select(".infobox").select("tr")
-
-    println(pageString)
+    val rows = Jsoup.parse(pageString).select(".infobox").select("tr").map { row ->
+        val cols = row.select("td")
+        if (cols.size != 2) null else {
+            val title = cols.first()!!.text()
+            val data = cols.last()!!.text()
+            println("title: $title, $data")
+            title to data
+        }
+    }
+    println(rows)
     return WikiData("")
 }
