8dbcfd2ac2eab67164c09f98c404e1a03d6aa1d9
ManApart <r@austinkucera.com>
2023-09-22 21:07:59 -0400

    basic wiki parsing

--- src/jvmMain/kotlin/wikiScraper/WikiData.kt
+++ src/jvmMain/kotlin/wikiScraper/WikiData.kt
@@ -1,3 +1,16 @@
 package wikiScraper
 
-data class WikiData(val temperature: String)
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class WikiData(
+    val type: String,
+    val temperature: String,
+    val atmosphere: String,
+    val magnetosphere: String,
+    val fauna: String,
+    val flora: String,
+    val water: String,
+    val resources: List<String>,
+    val traits: List<String>,
+)
--- src/jvmMain/kotlin/wikiScraper/WikiScraper.kt
+++ src/jvmMain/kotlin/wikiScraper/WikiScraper.kt
@@ -1,24 +1,36 @@
 package wikiScraper
 
+import jsonMapper
+import kotlinx.serialization.encodeToString
 import org.jsoup.Jsoup
+import java.io.File
 
 fun main() {
-    val planets = listOf("Earth")
-    planets
+    val planets = listOf("Mars")
         .map { getPage("https://starfieldwiki.net/wiki/Starfield:$it") }
         .map { parseWikiData(it) }
+    File("raw-data/wiki-data.json").writeText(jsonMapper.encodeToString(planets))
 }
 
 fun parseWikiData(pageString: String): WikiData {
-    val rows = Jsoup.parse(pageString).select(".infobox").select("tr").map { row ->
+    val data: Map<String, List<String>> = Jsoup.parse(pageString).select(".infobox").select("tr").mapNotNull { row ->
+        val title = row.selectFirst("th")?.text()?.trim()
         val cols = row.select("td")
-        if (cols.size != 2) null else {
-            val title = cols.first()!!.text()
-            val data = cols.last()!!.text()
-            println("title: $title, $data")
+        if (title == null || cols.isEmpty()) null else {
+            val data = cols.map { it.text().replace("◆", "").trim() }
             title to data
         }
-    }
-    println(rows)
-    return WikiData("")
+    }.toMap()
+    val resources = data["Resources"]?.flatMap { it.replace("  ", " ").split(" ") } ?: listOf()
+    return WikiData(
+        data["Type"]?.first() ?: "",
+        data["Temperature"]?.first() ?: "",
+        data["Atmosphere"]?.first() ?: "",
+        data["Magnetosphere"]?.first() ?: "",
+        data["Fauna"]?.first() ?: "",
+        data["Flora"]?.first() ?: "",
+        data["Water"]?.first() ?: "",
+        resources,
+        data["Traits"] ?: listOf(),
+    )
 }
