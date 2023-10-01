17fa48d62449bf2cfd065076e7c8f8c6a4001b40
ManApart <r@austinkucera.com>
2023-10-01 09:09:27 -0400

    add info on travel

--- src/jsMain/kotlin/views/AboutView.kt
+++ src/jsMain/kotlin/views/AboutView.kt
@@ -41,8 +41,9 @@ fun aboutView() {
 
                     p { +"The Crew page also links to similar sites made by others, so you can pick the one that works best for you." }
 
-                    p { +"Please report any issues as "
-                        a(href = "https://github.com/ManApart/starfield-eye/issues", target = "_blank"){+"github issues."}
+                    p {
+                        +"Please report any issues as "
+                        a(href = "https://github.com/ManApart/starfield-eye/issues", target = "_blank") { +"github issues." }
                     }
 
                 }
@@ -56,22 +57,24 @@ fun aboutView() {
                 }
                 div("section-view-box") {
                     h2 { +"Catalogue View" }
-                    div("accent-line"){+"Time dances its years forward"}
+                    div("accent-line") { +"Time dances its years forward" }
                     p { +"The catalogue gives you access to a powerful search. Here you can search for systems and planets using a large number of criteria." }
                     p { +"Comma separated, you can search for things like planet names, available resources (by symbol or name), planet outposts, and more. Comma separated terms are considered an \"AND\", so you can filter for planets with a warm temperature that include iron." }
                     p { +"Search results display the system and any matching planets. By hovering/tapping a result, you'll see a detailed view of that star or planet. You can then view it's system to see more information about that system" }
                 }
                 div("section-view-box") {
                     h2 { +"System View" }
-                    div("accent-line"){+"Do the meet and greets later"}
+                    div("accent-line") { +"Do the meet and greets later" }
                     p { +"Clicking on a system sends you to the system view. This view includes an interactive orrery that lets you browse the various bodies of the system." }
                     p { +"Navigate by clicking the star, planets, or moons, or use the arrow keys." }
                     p { +"You can add your own data to any planet, or moon. This includes labels that can be searched for, adding your outposts names (that can be searched by name or by searching for \"outpost\", or even notes for a specific planet." }
                     p { +"Data is saved locally to the browser and can be imported or exported from the Galaxy View." }
+                    p { +"Fast travel is in super alpha, but I'm releasing it in it's wip state. If you're connected to the game and have your in game starmap open, you can use this view and click the \"travel\" button. If everything works, your ship will set course to the planet you picked." }
+
                 }
                 div("section-view-box") {
                     h2 { +"Connecting to your Game" }
-                    div("accent-line"){+"And the darkness gets lit, just a candle more"}
+                    div("accent-line") { +"And the darkness gets lit, just a candle more" }
                     p { +"Thanks to the incredible work of Stonegdi, The Eye can connect to your game and grab all sorts of data. This transforms the site from an interactive map into (hopefully) a full fledged companion app." }
                     p { +"I want to favor speed over stability, so things may break as we go. I have a long personal wishlist of things to add as I figure things out or functionality is added to tooling. In the near term I hope to make a stats page and make the map interactively. Ideally you'll be able to search the site for a planet, and then click it to chart the course in the in game map." }
 
