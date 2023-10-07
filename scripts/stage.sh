00b700e95151549c5506b6418e2a568be368d0b7
ManApart <r@austinkucera.com>
2023-10-07 09:54:14 -0400

    quieter zipping

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -2,4 +2,4 @@
 cp -r build/dist/js/productionExecutable/* ../starfield-eye-deploy/
 cp -r build/dist/js/productionExecutable/* mod/Data/SFSE/plugins/sfse_plugin_console_web
 rm mod/starfield-eye.zip
-zip -r mod/starfield-eye.zip mod/Data
+zip -q -r mod/starfield-eye.zip mod/Data
